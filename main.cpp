edc3fa9e247bad54696f21783509cb86557798ce
cri-cov <cristian.covato@marconibari.edu.it>
2024-11-20 20:48:42 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+int main() {
+    int maxPosti(30), numAdulti(0), numBambini(0);
+    float postiOccupati(0), postiDaLiberare(0);
+    
+    do {
+        cout << "Quanti adulti entrano?" << endl;
+        cin >> numAdulti;
+        cout << "Quanti bambini salgono?" << endl;
+        cin >> numBambini;
+      
+        postiOccupati = postiOccupati + numAdulti + numBambini / 2.0;
+        cout << "Posti occupati: " << postiOccupati << endl;
+      
+    } while (postiOccupati < maxPosti);
+  
+    if (postiOccupati > maxPosti) {
+        postiDaLiberare = postiOccupati - maxPosti;
+        cout << "Liberare " << postiDaLiberare << " posti" << endl;
+    }
+    cout << "Testa allo schienale, si va!" << endl;
+}
